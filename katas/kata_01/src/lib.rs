12690501e7a7cf11f327581e9164c2acb61f0dbc
smile14 <smile.pakawat@gmail.com>
2026-02-12 16:44:15 +0700

    added kata 01 two_sum module

---
+++ katas/kata_01/src/lib.rs
@@ -0,0 +1,24 @@
+/// Returns indices of the two numbers such that they add up to target.
+///
+/// # Arguments
+///
+/// * `nums` - A vector of integers.
+/// * `target` - Target sum.
+///
+/// # Returns
+///
+/// `Some((i, j))` if found, otherwise `None`.
+///
+/// # Examples
+///
+/// ```
+/// use kata_01::two_sum;
+///
+/// let nums = vec![2, 7, 11, 15];
+/// let target = 9;
+/// let result = two_sum(nums, target);
+/// assert_eq!(result, Some((0, 1)));
+/// ```
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Option<(i32, i32)> {
+    None
+}
